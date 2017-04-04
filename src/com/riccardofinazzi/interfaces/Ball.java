819eda016c45c22976e8187f16487ff75f04af83
umadnomad <umadnomad@users.noreply.github.com>
2017-04-05 00:16:06 +0200

    Page 121: In order to be a legal implementation class, a nonabstract implementation class must declare no checked exceptions on implementation methods other than those declared by the interface method, or subclasses of those declared by the interface method

--- src/com/riccardofinazzi/interfaces/Ball.java
+++ src/com/riccardofinazzi/interfaces/Ball.java
@@ -1,15 +1,26 @@
 package com.riccardofinazzi.interfaces;
 
+class BroaderException extends Exception {};
+class IntermediateException extends BroaderException {};
+class SmallerException extends IntermediateException {};
+
+class OtherException extends Exception {};
+
 interface Bounceable {
 	
-	void bounce();
-	void setBounceFactor(int bf);
+	void bounce() throws IntermediateException;
+	//void setBounceFactor(); /* Change method signature to take an int parameter */
 	
 }
 
 public class Ball implements Bounceable {
 	
-	public void bounce() {}
+	/* As you can see as long as our implementing method declares an exception whic
+	 * h is a subclass of the exception thrown by the method inherited from the int
+	 * erface its all okay */
+	public void bounce() throws SmallerException {}
+	//public void bounce() throws BroaderException {} /* Won't compile ! */
+	
 	/* In the book, at page 120, it says this code is supposed to compile but I har
 	 * dly believe so as the method is set to take an int parameter that was not de
 	 * clared in the interface contract */
