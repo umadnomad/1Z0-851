1ec0357fdc8b1ebeadbb4011d55016bdfe3fae56
umadnomad <umadnomad@users.noreply.github.com>
2017-04-06 14:58:36 +0200

    Testing book's page 146

---
+++ src/com/riccardofinazzi/FinalClassVar.java
@@ -0,0 +1,14 @@
+class Foo {
+	static final int test; // this var must be finalized before constructor ends
+	/*
+		As expected:
+		error: variable test not initialized in the default constructor
+		static final int test;
+		*/
+}
+
+class FooTestDrive {
+	public static void main(String[] args) {
+		new Foo();
+	}
+}
