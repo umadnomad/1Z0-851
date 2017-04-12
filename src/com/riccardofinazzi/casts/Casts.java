16e9a71a8d2f88a89e2a2e1cb3bb80814c6698db
umadnomad <umadnomad@users.noreply.github.com>
2017-04-12 14:24:59 +0200

    Added book's page 207 example

--- src/com/riccardofinazzi/casts/Casts.java
+++ src/com/riccardofinazzi/casts/Casts.java
@@ -47,4 +47,27 @@ class TestCasts {
 		c = (byte) (b + c); // Works
 		c += b;				// Works and is equivalent
 	}
-}
+	
+	/* I am want to add this whole book's example and note, available at page 207:
+	 * 
+	 *	public class TimeTravel {
+	 *		public static void main(String[] args) {
+	 *		int year = 2050;
+	 *		System.out.println("The year is " + year);
+	 *		}
+	 *	}
+	 * 
+	 * Local variables, including primitives, always, always, always must be initia
+	 * lized _before_ you attempt to use them (though not necessarily on the same l
+	 * ine of code).
+	 * Java does not give local variables a default value; you must explicitly init
+	 * ialize them with a value, as in the preceding example. If you try to use an 
+	 * unitialized primitive in your code, you'll get a compiler error:
+	 *
+	 *	public class TimeTravel {
+	 *		public static void main(String[] args) {
+	 *		int year;
+	 *		System.out.println("The year is " + year); // Compiler error!!
+	 *		}
+	 *	}
+	*/	
